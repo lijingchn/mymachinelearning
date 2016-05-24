c9b0c49db94e3af05fe8a3d4dd4f1fe15e8951d2
lijingchn <lijingjing.chn@gmail.com>
2016-05-24 15:54:58 +0800

    my random forest.

--- titanic/mycode/random_forest.py
+++ titanic/mycode/random_forest.py
@@ -4,18 +4,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 train_df = pd.read_csv("../data/train.csv", dtype={"Age": np.float64})
 test_df = pd.read_csv("../data/test.csv", dtype={"Age": np.float64})
-
-#train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-#test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
-#train_df = pd.read_csv(train_url)
-
-train_df["Child"] = 0
-train_df["Child"][train_df["Age"]<18] = 1
 
 # fill the missingness data
 train_df["Age"] = train_df["Age"].fillna(train_df["Age"].median())
@@ -28,22 +20,15 @@ train_df["Sex"][train_df["Sex"] == "male"] = 1
 train_df["Embarked"][train_df["Embarked"] == "S"] = 0
 train_df["Embarked"][train_df["Embarked"] == "C"] = 1
 train_df["Embarked"][train_df["Embarked"] == "Q"] = 2
-#print train_df
 train_df["Family_size"] = train_df["Parch"] + train_df["SibSp"] + 1
 
-# drow the picture
-plt.figure()
-sns.pairplot(data=train_df[["Family_size","Survived","Sex","Age"]],hue="Survived",dropna=True)
-plt.show()
-
-max_depth = 10
-min_samples_split = 5
+# Building and fitting my forest
 target = train_df["Survived"].values
-features = train_df[["Family_size","Pclass","Sex","Age","Fare"]].values
-my_tree = tree.DecisionTreeClassifier(max_depth=max_depth,min_samples_split=min_samples_split, random_state=1)
-my_tree = my_tree.fit(features,target)
-#print my_tree.feature_importances_
-print my_tree.score(features,target)
+features_forest = train_df[["Family_size","Pclass","Sex","Age","Fare","SibSp", "Parch", "Embarked"]].values
+forest = RandomForestClassifier(n_estimators=100, n_jobs=4, max_features="sqrt")
+my_forest = forest.fit(features_forest,target)
+print "======================================"
+print my_forest.score(features_forest,target)
 
 #print train_df["Survived"][train_df["Child"] == 1].value_counts()
 #print train_df["Child"][train_df["Child"]==1]
@@ -55,9 +40,13 @@ test_df["Fare"] = test_df["Fare"].fillna(test_df["Fare"].median())
 test_df["Age"] = test_df["Age"].fillna(test_df["Fare"].median())
 test_df["Sex"][test_df["Sex"] == "female"] = 0
 test_df["Sex"][test_df["Sex"] == "male"] =1
+test_df["Embarked"][test_df["Embarked"] == "S"] = 0
+test_df["Embarked"][test_df["Embarked"] == "C"] = 1
+test_df["Embarked"][test_df["Embarked"] == "Q"] = 2
 test_df["Family_size"] = test_df["Parch"] + test_df["SibSp"] + 1
-test_features = test_df[["Family_size","Pclass","Sex","Age","Fare"]].values
-my_prediction = my_tree.predict(test_features)
+
+test_features = test_df[["Family_size","Pclass","Sex","Age","Fare","SibSp", "Parch", "Embarked"]].values
+my_prediction = my_forest.predict(test_features)
 
 # Create a data frame with two columns: PassengerId & Survived.
 PassengerId = np.array(test_df["PassengerId"]).astype(int)
@@ -66,5 +55,5 @@ my_solution = pd.DataFrame(my_prediction, PassengerId, columns=["Survived"])
 #print my_solution.shape
 
 # Write solution to a csv file with the name my_solution.csv
-my_solution.to_csv("my_solution.csv", index_label=["PassengerId"])
+my_solution.to_csv("my_forest_solution.csv", index_label=["PassengerId"])
 
