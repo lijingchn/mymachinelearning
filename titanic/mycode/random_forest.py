fae6132d38d21c09e1efb4a2d4bd429f12dd5b60
lijingchn <lijingjing.chn@gmail.com>
2016-05-29 22:51:35 +0800

    parameter select

--- titanic/mycode/random_forest.py
+++ titanic/mycode/random_forest.py
@@ -136,6 +136,142 @@ plt.figure(figsize=(15,7))
 plt.subplot(2,3,1)
 feature_param = ["auto","sqrt","log2",None]
 scores = []
+for feature in feature_param:
+    clf = DecisionTreeClassifier(max_features=feature)
+    clf.fit(X_train,y_train)
+    scoreCV = sklearn.cross_validation.cross_val_score(clf,X,y,cv=10)
+    scores.append(np.mean(scoreCV))
+    print '================='
+    print scores
+plt.plot(scores,'.-')
+plt.axis('tight')
+plt.xlabel('parameter')
+plt.ylabel('score')
+plt.title('Max Features')
+plt.xticks(range(len(feature_param)),feature_param)
+plt.grid()
+#plt.show()
+
+# Max Depth
+plt.subplot(2,3,2)
+feature_param = range(1,51)
+scores = []
+for feature in feature_param:
+    clf = DecisionTreeClassifier(max_depth=feature)
+    clf.fit(X_train,y_train)
+    scoreCV = sklearn.cross_validation.cross_val_score(clf,X,y,cv=10)
+    scores.append(np.mean(scoreCV))
+plt.plot(feature_param,scores,".-")
+plt.axis('tight')
+plt.xlabel('parameter')
+plt.ylabel('score')
+plt.title('Max Depth')
+plt.grid()
+#plt.show()
+
+# Min Samples Split
+plt.subplot(2,3,3)
+feature_param = range(1,51)
+scores = []
+for feature in feature_param:
+    clf = DecisionTreeClassifier(min_samples_split = feature)
+    clf.fit(X_train,y_train)
+    scoreCV = sklearn.cross_validation.cross_val_score(clf,X,y,cv=10)
+    scores.append(np.mean(scoreCV))
+plt.plot(feature_param,scores,'.-')
+plt.axis('tight')
+plt.xlabel('parameter')
+plt.ylabel('score')
+plt.title('Min Samples Split')
+plt.grid()
+#plt.show()
+
+# Min Samples Leaf
+plt.subplot(2,3,4)
+feature_param = range(1,51)
+scores = []
+for feature in feature_param:
+    clf = DecisionTreeClassifier(min_samples_leaf = feature)
+    clf.fit(X_train,y_train)
+    scoreCV = sklearn.cross_validation.cross_val_score(clf,X,y,cv=10)
+    scores.append(np.mean(scoreCV))
+plt.plot(feature_param,scores,'.-')
+plt.axis('tight')
+plt.xlabel('parameter')
+plt.ylabel('score')
+plt.title('Min Samples Leaf')
+plt.grid()
+#plt.show()
+
+# Min Weight Fraction Leaf
+plt.subplot(2,3,5)
+feature_param = np.linspace(0,0.5,10)
+scores = []
+for feature in feature_param:
+    clf = DecisionTreeClassifier(min_weight_fraction_leaf = feature)
+    clf.fit(X_train,y_train)
+    scoreCV = sklearn.cross_validation.cross_val_score(clf,X,y,cv=10)
+    scores.append(np.mean(scoreCV))
+plt.plot(feature_param,scores,'.-')
+plt.axis('tight')
+plt.xlabel('parameter')
+plt.ylabel('score')
+plt.title('Min Weight Fraction Leaf')
+plt.grid()
+#plt.show()
+
+#Max Leaf Nodes
+plt.subplot(2,3,6)
+feature_param = range(2,21)
+scores = []
+for feature in feature_param:
+    clf = DecisionTreeClassifier(max_leaf_nodes = feature)
+    clf.fit(X_train,y_train)
+    scoreCV = sklearn.cross_validation.cross_val_score(clf,X,y,cv=10)
+    scores.append(np.mean(scoreCV))
+plt.plot(feature_param,scores,'.-')
+plt.axis('tight')
+plt.xlabel('paramter')
+plt.ylabel('score')
+plt.title("Max Leaf Nodes")
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
